18e7c5e8501ef06625919cd899216343281e92e4
Ahtem124 <sugrob.22@mail.ru>
2023-02-07 16:28:30 +0300

    Задача тре

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-let t = 20;
-if (t<-30) {
-    console.log("Останься дома!");
-} else if (t< -10 && t>-30) {
-    console.log ("сегодня холодно");
-} else if (t>=5 && t<20) {
-    console.log ("тепло")
-} else if (t >= 20 ) {
-    console.log ("жара")
+let t = 1
+if (t = 0 && t > 0) {
+    console.log ("верно")
 } else {
-    console.log ("ад")
+    console.log ("неверно")
 }
-
-
-  
